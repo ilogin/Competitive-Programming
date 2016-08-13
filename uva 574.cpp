f14cb2a238148dfdbb2d38295b899845b7c70775
Raymond <1107450@gmail.com>
2016-08-12 23:06:16 -0700

    Unfinished recursion problem

---
+++ uva 574.cpp
@@ -0,0 +1,22 @@
+/*
+ID: 1107501
+TASK: milk2
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+#define forit(it, a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); it++)
+#define pb push_back
+#define mp make_pair
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+int main(){
+
+	return 0;
+}
